9f51a81d88fa63f9585125f3aba43dadabfbaeaf
Dean Burgess <dinocastiglione@ymail.com>
2019-07-29 12:33:08 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -50,6 +50,6 @@ def using_delete(perfect, wake_up)
   perfect.delete(wake_up)
 end
 
-def using_delete_at(famous_robots, dino)
-  famous_robots.delete(2)
+def using_delete_at(famous_robots, 2)
+  famous_robots.delete(1)
 end
