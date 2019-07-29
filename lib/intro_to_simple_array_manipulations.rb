e3033f27782eb4e53acae9c49f55f95b5d585907
Dean Burgess <dinocastiglione@ymail.com>
2019-07-29 12:47:00 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,55 @@
+=begin
+def 
+  colors_in_the_rainbow = ["red", "orange", "yellow", "green", "blue", "indigo"]
+  next_color = "violet"
+  colors_in_the_rainbow.push(next_color)
+end 
+=end
+
+def using_push(colors_in_the_rainbow, next_color)
+  colors_in_the_rainbow.push(next_color)
+end
+
+def using_unshift(array, next_color)
+  array.unshift(next_color)
+end
+
+def using_pop(britney)
+  britney.pop
+end
+
+def pop_with_args(janet)
+  janet.pop(2)
+end 
+
+def using_shift(alanis)
+  alanis.shift
+end
+
+def shift_with_args(whitney)
+  whitney.shift(2)
+end
+
+def using_concat(array1, array2)
+array1.concat(array2)
+end
+
+def using_insert(invitation, make_me)
+  invitation.insert(4, "Python")
+end
+
+def using_uniq(womanizer)
+  womanizer.uniq
+end
+
+def using_flatten(all_that)
+  all_that.flatten
+end
+
+def using_delete(perfect, wake_up)
+  perfect.delete(wake_up)
+end
+
+def using_delete_at(famous_robots, word)
+  famous_robots.delete_at(2)
+end
