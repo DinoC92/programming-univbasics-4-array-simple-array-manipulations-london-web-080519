fead0c1d6bcccf6747202cc710f6859ef5a8cb70
Dean Burgess <dinocastiglione@ymail.com>
2019-07-29 11:10:25 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,35 @@
+=begin
+def 
+  colors_in_the_rainbow = ["red", "orange", "yellow", "green", "blue", "indigo"]
+  next_color = "violet"
+  colors_in_the_rainbow.push(next_color)
+end 
+=end
+
+def using_push(colors_in_the_rainbow, next_color)
+  colors_in_the_rainbow.push(next_color)
+end
+
+def using_unshift(array, next_color)
+  array.unshift(next_color)
+end
+
+def using_pop(britney)
+  britney.pop
+end
+
+def pop_with_args(janet)
+  janet.pop(2)
+end 
+
+def using_shift(alanis)
+  alanis.shift
+end
+
+def shift_with_args(whitney)
+  whitney.shift(2)
+end
+
+def using_concat(array1, array2)
+array1.concat(array2)
+end 
